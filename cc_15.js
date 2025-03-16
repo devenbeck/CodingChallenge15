c1a570ec0437149baee1bb44ce6661a565919633
Natalie Devenbeck <devenbeck@usf.edu>
2025-03-16 19:03:59 -0400

    Task 1- Base Structure Setup

---
+++ cc_15.js
@@ -0,0 +1,6 @@
+//Task 1- After html setup, select the riskDashboard container 
+//And print "Risk Dashboard Loaded" to the console
+
+document.addEventListener('DOMContentLoaded', function(){
+    console.log("Risk Dashboard Loaded");
+})
